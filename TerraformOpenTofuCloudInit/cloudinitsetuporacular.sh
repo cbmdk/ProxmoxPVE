03143388aa72492b53dca0d775b18a66f421dfac
Christian Buchhave Mortensen <cbm@cbm.dk>
2025-03-13 10:40:00 +0100

    Update cloudinitsetuporacular.sh

--- TerraformOpenTofuCloudInit/cloudinitsetuporacular.sh
+++ TerraformOpenTofuCloudInit/cloudinitsetuporacular.sh
@@ -16,10 +16,10 @@ qm create 9000 --memory 2048 --net0 virtio,bridge=vmbr0 --scsihw virtio-scsi-pci
 
 # import the downloaded disk to the local-lvm storage, attaching it as a SCSI drive
 # note that import path must be a full path, not relative
-qm set 9000 --scsi0 local-lvm:0,import-from=/root/oracular-server-cloudimg-amd64.img
+qm set 9000 --scsi0 storage:0,import-from=/root/oracular-server-cloudimg-amd64.img
 
 # set boot and display options
-qm set 9000 --ide2 local-lvm:cloudinit
+qm set 9000 --ide2 storage:cloudinit
 qm set 9000 --serial0 socket --vga serial0
 
 # very important - had to be updated in the image
